f496b37488476e1a1a4232303479208e72c2c24e
Chu Kim Thang <chukimthang94@gmail.com>
2017-09-19 16:57:29 +0700

    Updating, showing and deleting users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create name: "Chu Kim Thang", email: "chukimthang94@gmail.com", 
+  password: "123456", password_confirmation: "123456", admin: true
+
+User.create name: "Kim Thang Chu", email: "thangck94@gmail.com", 
+  password: "123456", password_confirmation: "123456"
+
+40.times do |n|
+  name = Faker::Name.name
+  email = "email#{n}@email.com"
+  password = "123456"
+  password_confirmation = "123456"
+
+  User.create name: name, email:email, password: password, password_confirmation: password_confirmation
+end
